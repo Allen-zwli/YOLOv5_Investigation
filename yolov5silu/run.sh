602ee86bedcaf3a10778b0d51feb845b7a2364e2
Allen-zwli <83260789+Allen-zwli@users.noreply.github.com>
2021-04-30 21:57:22 +0800

    Update run.sh

--- yolov5silu/run.sh
+++ yolov5silu/run.sh
@@ -1,4 +1,6 @@
-python train.py --img 640 --batch 128 --epochs 250 --data voc.yaml --cfg yolov5sTRlast.yaml --weights '' --cache --name exp_yolov5s_voc_init_TRlast
-# python train.py --img 640 --batch 128 --epochs 150 --data voc.yaml --weights yolov5s.pt --cache
-python train.py --img 640 --batch 128 --epochs 250 --data voc.yaml --cfg yolov5sTRlast2L.yaml --weights '' --cache --name exp_yolov5s_voc_init_TRlast2L
-python train.py --img 640 --batch 128 --epochs 250 --data voc.yaml --cfg yolov5sTRlastbone.yaml --weights '' --cache --name exp_yolov5s_voc_init_TRlastbone
+python train.py --img 640 --batch 128 --epochs 150 --data voc.yaml --weights yolov5s.pt --cache --hyp data/hyp.finetune.yaml --name exp_yolov5s_silu_finetune
+python train.py --img 640 --batch 128 --epochs 150 --data voc.yaml --cfg yolov5s.yaml --weights '' --cache --name exp_yolov5s_silu_scratch
+
+python train.py --img 640 --batch 128 --epochs 250 --data voc.yaml --cfg yolov5sTRlast.yaml --weights '' --cache --name exp_yolov5s_scratch_TRlast
+python train.py --img 640 --batch 128 --epochs 250 --data voc.yaml --cfg yolov5sTRlast2L.yaml --weights '' --cache --name exp_yolov5s_scratch_TRlast2L
+python train.py --img 640 --batch 128 --epochs 250 --data voc.yaml --cfg yolov5sTRlastbone.yaml --weights '' --cache --name exp_yolov5s_scratch_TRlastbone
