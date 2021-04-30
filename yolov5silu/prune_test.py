a76d162fd32d72e686b73ad714c78417f3347a04
Allen-zwli <83260789+Allen-zwli@users.noreply.github.com>
2021-04-30 21:59:11 +0800

    Update prune_test.py

--- yolov5silu/prune_test.py
+++ yolov5silu/prune_test.py
@@ -6,7 +6,7 @@ pr_thres=np.linspace(0,0.7,100)
 
 for pr in pr_thres:
     t = time.time()
-    sh = 'python test.py --weights ./runs/train/exp_yolov5s_finetune/weights/best.pt --data voc.yaml --img 640 --batch 128 --iou 0.6 --name prune%.3f --prune-rate %.3f --conf 0.01'% (pr,pr)
+    sh = 'python test.py --weights ./runs/train/exp_yolov5s_finetune/weights/best.pt --data voc.yaml --img 640 --batch 128 --iou 0.6 --write-prune --name prune%.3f --prune-rate %.3f --conf 0.01'% (pr,pr)
     os.system(sh)
     with open('time.txt','a') as f:
         f.write(str(round(time.time()-t,2))+'\n')
